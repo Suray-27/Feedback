3a565dd5a1274555906d0d489f382714e299e2f2
Surendhar <162092531+Suray-27@users.noreply.github.com>
2024-12-20 13:17:07 +0000

    commit

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from dotenv import load_dotenv
+import streamlit as st
+from ai_response import generate_reply
+
+st.title("Customer reply generator")
+
+user_review = st.text_area("Enter Your Review:")
+sentiment = st.selectbox("Select Sentiment", ["positive", "negative","neutral"])
+
+if st.button("Generate Reply"):
+    reply = generate_reply(user_query=user_review, sentiment=sentiment)
+    st.write(reply)
+else:
+    st.write("Please a enter a review and select a sentiment to generate a reply.")
 
 
 
-load_dotenv()
+
