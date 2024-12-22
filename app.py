fd31aa7d3d0b581f9b384dddf034131d1841544a
Surendhar <162092531+Suray-27@users.noreply.github.com>
2024-12-22 09:45:03 +0000

    app comit

--- app.py
+++ app.py
@@ -3,46 +3,48 @@ from pymongo import MongoClient
 from ai_response import generate_reply
 from email_utils import email_coupon 
 
-# Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/<feedback_db>")
-db = client["feedback_db"]
-collection = db["reviews"]
+
 
 st.title("Customer reply generator")
 
-st.sidebar.header("User Login")
-user_email = st.sidebar.text_input("Enter Your Email:", placeholder="eg., user@gmail.com")
+st.header("User Login")
+user_email = st.text_input("Enter Your Email:", placeholder="eg., user@gmail.com")
 
-if st.sidebar.button("Login"):
-    if not user_email.strip():
-        st.sidebar.error("Please enter a valid email.")
-    else:
-        if not collection.find_one({"user_email": user_email}):
-            collection.insert_one({"user_email": user_email})
-            st.sidebar.success("User logged in successfully.")
-
-user_review = st.text_area("Enter Your Review:",placeholder="Write your review here...")
-sentiment = st.selectbox("Select Sentiment", ["positive", "negative","neutral"])
-
-if st.button("Generate Reply"):
-    if not user_review.strip():
-        st.error("Please enter a review.")
+if st.button("Login"):
+    if (user_email == "" or "@gmail.com" not in user_email):
+        st.error("Please enter a valid email.")
         st.stop()
     else:
-        reply = generate_reply(user_query=user_review, sentiment=sentiment)
-        st.write(reply)
+        st.session_state.logged_in = True
+        #st.session_state.user_email = user_email
+        st.success("User logged in successfully.")
 
-    if sentiment == "negative":
-        email_coupon("50% off on your next purchase",
-                     "Dear Customer,you have been selected to receive a 50% discount on your next purchase. Use the code: SAVE50 at checkout.",
-                     "yyin42171@gmail.com")
+if st.session_state:
 
-    # Save the review and reply to MongoDB
-    collection.insert_one({"review": user_review, "reply": reply, "sentiment": sentiment})
-    st.success("Reply generated and saved to MongoDB.")
+    user_review = st.text_area("Enter Your Review:",placeholder="Write your review here...")
+    sentiment = st.selectbox("Select Sentiment", ["positive", "negative","neutral"])
 
-else:
-    st.write("Please a enter a review and select a sentiment to generate a reply.")
+    if st.button("Generate Reply"):
+        if not user_review.strip():
+            st.error("Please enter a review.")
+            st.stop()
+        else:
+            reply = generate_reply(user_query=user_review, sentiment=sentiment)
+            st.write(reply)
+
+            if sentiment == "negative":
+                email_coupon("50% off on your next purchase",
+                        "Dear Customer,you have been selected to receive a 50% discount on your next purchase. Use the code: SAVE50 at checkout.",
+                        "yyin42171@gmail.com")
+            elif sentiment == "neutral":
+                email_coupon("25% off on your next purchase",
+                        "Dear Customer, you have been selected to receive a 25% discount on your next purchase. Use the code: SAVE25 at checkout.",
+                        "surendharbaskar3@gmail.com")
+
+
+
+    else:
+        st.write("Please log in to enter a review and generate a reply.")
 
 
 
