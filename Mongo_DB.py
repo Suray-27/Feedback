995aed632bd244811d715963622a81d5473d1485
Surendhar <162092531+Suray-27@users.noreply.github.com>
2024-12-22 14:48:43 +0000

    80% app.py complete

--- Mongo_DB.py
+++ Mongo_DB.py
@@ -1,28 +1,18 @@
 from pymongo import MongoClient
-from urllib.parse import quote_plus
 import pandas as pd
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
-password = quote_plus(os.getenv("MONGODB_PASSWORD"))
-
-try:
-    client = MongoClient(f"mongodb+srv://capstone:{password}@capstone.sb2ut.mongodb.net/?retryWrites=true&w=majority&appName=capstone")
-    print("Connected to MongoDB!")
-    print("Databases:", client.list_database_names())
-except Exception as e:
-    print("Error:", e)
+client = MongoClient(os.getenv("MONGODB_PASSWORD"))
+db = client["feedback_db"]
+collection = db["reviews"]
 
 
-
-"""client = MongoClient("mongodb://localhost:27017/")
-print("Databases on server:", client.list_database_names())
 class MongoDB:
     def __init__(self):
-        self.client = MongoClient("mongodb://localhost:27017/")
+        self.client = MongoClient(os.getenv("MONGODB_PASSWORD"))
         self.db = self.client["customer_reviews"]
         self.collection = self.db["reviews"]
 
@@ -36,6 +26,3 @@ class MongoDB:
 
 get_reviews = MongoDB().get_reviews()
 
-if __name__ == "__main__":
-    reviews_df = get_reviews
-    print(reviews_df)"""
