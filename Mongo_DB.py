5faadb114c1b4ecf94ef21af23c0b376fdfb88e5
Surendhar <162092531+Suray-27@users.noreply.github.com>
2024-12-22 15:16:28 +0000

    update

--- Mongo_DB.py
+++ Mongo_DB.py
@@ -9,20 +9,8 @@ client = MongoClient(os.getenv("MONGODB_PASSWORD"))
 db = client["feedback_db"]
 collection = db["reviews"]
 
+cursor = collection.find()
 
-class MongoDB:
-    def __init__(self):
-        self.client = MongoClient(os.getenv("MONGODB_PASSWORD"))
-        self.db = self.client["customer_reviews"]
-        self.collection = self.db["reviews"]
-
-    def save_review_reply(self, user_review, reply, sentiment):
-        self.collection.insert_one({"review": user_review, "reply": reply, "sentiment": sentiment})
-
-    def get_reviews(self):
-        reviews = self.collection.find({}, {"_id": 0})
-        return pd.DataFrame(list(reviews))
-    
-
-get_reviews = MongoDB().get_reviews()
+for record in cursor:
+    print(record)
 
