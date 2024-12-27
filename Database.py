11acf66e4d08ffd3f16d051285d77fb287d9b301
Suray-27 <surendharb14@gmail.com>
2024-12-27 14:33:17 +0530

    renamed

---
+++ Database.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient # Required Library
+import os
+from io import BytesIO
+import joblib
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Initiate Database
+client = MongoClient(os.getenv("MONGODB_PASSWORD")) # Credentials
+db = client["Model_db"]
+collection = db["models"]
+
+def load_model_from_mongodb(model_name):
+    # fetch the model from mongodb
+
+    model_data = collection.find_one({"name": model_name})
+    if not model_data:
+        raise ValueError(f"Model {model_name} not found in MongoDB.")
+
+    # Deserialize the binary data
+    model = joblib.load(BytesIO(model_data["data"]))
+    return model
