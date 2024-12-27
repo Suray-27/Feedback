b3782e128fe9347ae092ebe9c047eb8552678bf1
Surendhar <162092531+Suray-27@users.noreply.github.com>
2024-12-27 17:12:28 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import nltk
-from nltk.corpus import stopwords
-from pre_process import preprocess
-from email_utils import email_coupon
-from mongo_db import load_model_from_mongodb
-from ai_response import generate_reply
-
-# Use stopwords in your app
-stop_words = stopwords.words('english')
