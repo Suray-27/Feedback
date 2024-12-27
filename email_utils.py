f71942ff56e9935e7223a26ed89154048c265a6a
Suray-27 <surendharb14@gmail.com>
2024-12-27 15:02:10 +0530

    comments

--- email_utils.py
+++ email_utils.py
@@ -1,10 +1,11 @@
-import smtplib
+import smtplib # Required Libraries
 from email.message import EmailMessage
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# Initialise Email system
 def email_coupon(subject, body, to):
     msg = EmailMessage()
     msg.set_content(body)
@@ -17,7 +18,7 @@ def email_coupon(subject, body, to):
 
     msg['from'] = user
 
-    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465) # Gmail port
     server.login(user, password)
 
     server.send_message(msg)
