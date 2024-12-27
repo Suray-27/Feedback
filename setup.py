9b1bfc37598f8e0d7f97e49a3925f8e4a7e5d6dd
Suray-27 <surendharb14@gmail.com>
2024-12-27 16:43:13 +0530

    set

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import nltk
 from nltk.corpus import stopwords
+import pre_process
+import email_utils
+import mongo_db
+import ai_response
 
 # Use stopwords in your app
 stop_words = stopwords.words('english')
